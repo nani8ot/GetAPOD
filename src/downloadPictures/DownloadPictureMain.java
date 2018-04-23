8f67d9a085fdc1dd48fc255ad782ec6e247eb2e0
BachUhr <38629316+BachUhr@users.noreply.github.com>
2018-04-23 20:23:57 +0200

    Added Exception-Dialog in DownloadPictureMain

--- src/downloadPictures/DownloadPictureMain.java
+++ src/downloadPictures/DownloadPictureMain.java
@@ -5,26 +5,36 @@ import java.io.PrintWriter;
 import java.net.URL;
 import java.util.Date;
 
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import easyFiles.ReadProperties;
 
 public class DownloadPictureMain {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		String propertiesPath = "GetAPOD.conf";
 		String logPath = "GetApod.log";
 		
 		System.out.println("Working Directory = " + System.getProperty("user.dir")); // debug
 		
-		String urlLine = new URLReader().getUrlFileLine(new ReadProperties().getProperties(propertiesPath, "keyword"), new URL(new ReadProperties().getProperties(propertiesPath, "url")), propertiesPath);
-		
-		System.out.println(urlLine); // debug
-		
-		final long timeStart = System.currentTimeMillis();
-		new DownloadFile().getFile(urlLine, propertiesPath);
-		final long timeEnd = System.currentTimeMillis();
-		PrintWriter pWriter = new PrintWriter(new FileWriter(logPath, true), true);
-		pWriter.printf("%s: Downloads %s to %s\\%s in %s Seconds\n", new Date().toString(),urlLine, System.getProperty("user.dir").toString(), logPath, (timeEnd - timeStart) / 1000);
-		pWriter.close();
+		String urlLine;
+		try {
+			urlLine = new URLReader().getUrlFileLine(new ReadProperties().getProperties(propertiesPath, "keyword"), new URL(new ReadProperties().getProperties(propertiesPath, "url")), propertiesPath);
+			
+			System.out.println(urlLine); // debug
+			
+			final long timeStart = System.currentTimeMillis();
+			new DownloadFile().getFile(urlLine, propertiesPath);
+			final long timeEnd = System.currentTimeMillis();
+			PrintWriter pWriter = new PrintWriter(new FileWriter(logPath, true), true);
+			pWriter.printf("%s: Downloads %s to %s\\%s in %s Seconds\n", new Date().toString(),urlLine, System.getProperty("user.dir").toString(), logPath, (timeEnd - timeStart) / 1000);
+			pWriter.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(new JFrame(), e.toString().substring( e.toString().lastIndexOf(":") + 1, e.toString().length()), e.toString().substring(0, e.toString().lastIndexOf(":")), JOptionPane.ERROR_MESSAGE);
+		}
 		
 		System.out.println("Finished and Exit");
 	}
